016ff61f8dff2549bbe77290abc5bb2f371ecd0a
huda-salam <38382236+huda-salam@users.noreply.github.com>
2026-09-17 21:18:20 +0700

    fix vite production build output for Laravel

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
+  build: {
+    outDir: 'public/build',
+    manifest: true,
+    emptyOutDir: true,
+  },
+  server: {
+    port: 5173,
+  },
 });
