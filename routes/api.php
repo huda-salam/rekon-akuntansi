85f4b0a5da1cfd4ff2da6bda2458636fc520a985
huda-salam <38382236+huda-salam@users.noreply.github.com>
2026-09-17 14:46:05 +0700

    feat: expose master and source APIs

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,24 @@
 <?php
 
+use App\Http\Controllers\AccountingYearController;
+use App\Http\Controllers\AuthorizationController;
+use App\Http\Controllers\HealthController;
 use App\Http\Controllers\ReconciliationController;
+use App\Http\Controllers\SkpdController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/health', HealthController::class);
+
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/years', [AccountingYearController::class, 'index']);
+    Route::post('/years', [AccountingYearController::class, 'store']);
+    Route::post('/years/{accountingYear}/activate', [AccountingYearController::class, 'activate']);
+
+    Route::apiResource('/skpds', SkpdController::class)->only(['index','store','update']);
+
+    Route::get('/authorizations', [AuthorizationController::class, 'index']);
+    Route::post('/authorizations', [AuthorizationController::class, 'store']);
+
     Route::get('/reconciliations', [ReconciliationController::class, 'index']);
     Route::get('/reconciliations/{reconciliation}', [ReconciliationController::class, 'show']);
     Route::post('/reconciliations/{reconciliation}/finalize', [ReconciliationController::class, 'finalize']);
