0be2f68ecd1b4f175744ff9364fccea81902cacb
huda-salam <38382236+huda-salam@users.noreply.github.com>
2026-09-17 21:21:43 +0700

    Fix Vite manifest path for Laravel

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'public/build',
-    manifest: true,
+    manifest: 'manifest.json',
     emptyOutDir: true,
   },
   server: {
