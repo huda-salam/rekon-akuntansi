99f4a74172b23e508bf2b6bde542bf8879411abf
huda-salam <38382236+huda-salam@users.noreply.github.com>
2026-09-17 19:19:19 +0700

    Fix controller authorization support

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
 abstract class Controller
 {
-    // Base controller for application HTTP controllers.
+    use AuthorizesRequests;
 }
