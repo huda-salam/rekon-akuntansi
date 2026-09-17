8696d9ee0de1e5d83649b53f3a9a8d553c7cacd9
huda-salam <38382236+huda-salam@users.noreply.github.com>
2026-09-17 14:53:59 +0700

    feat: expose stable authentication and reconciliation APIs

--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,33 @@
 <?php
 
 use App\Http\Controllers\AccountingYearController;
-use App\Http\Controllers\AuthorizationController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AuthorizationSourceController;
 use App\Http\Controllers\HealthController;
 use App\Http\Controllers\ReconciliationController;
-use App\Http\Controllers\SkpdController;
+use App\Http\Controllers\ReconciliationCrudController;
+use App\Http\Controllers\SkpdAdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/health', HealthController::class);
+Route::post('/auth/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/auth/me', [AuthController::class, 'me']);
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+
     Route::get('/years', [AccountingYearController::class, 'index']);
     Route::post('/years', [AccountingYearController::class, 'store']);
     Route::post('/years/{accountingYear}/activate', [AccountingYearController::class, 'activate']);
 
-    Route::apiResource('/skpds', SkpdController::class)->only(['index','store','update']);
+    Route::apiResource('/skpds', SkpdAdminController::class)->only(['index', 'store', 'update']);
 
-    Route::get('/authorizations', [AuthorizationController::class, 'index']);
-    Route::post('/authorizations', [AuthorizationController::class, 'store']);
+    Route::get('/authorizations', [AuthorizationSourceController::class, 'index']);
+    Route::post('/authorizations', [AuthorizationSourceController::class, 'store']);
 
     Route::get('/reconciliations', [ReconciliationController::class, 'index']);
+    Route::post('/reconciliations', [ReconciliationCrudController::class, 'store']);
     Route::get('/reconciliations/{reconciliation}', [ReconciliationController::class, 'show']);
+    Route::put('/reconciliations/{reconciliation}', [ReconciliationCrudController::class, 'update']);
     Route::post('/reconciliations/{reconciliation}/finalize', [ReconciliationController::class, 'finalize']);
 });
