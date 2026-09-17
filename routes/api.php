ec973752aff695524063cd85df7b9a31f1908ac0
huda-salam <38382236+huda-salam@users.noreply.github.com>
2026-09-17 20:43:57 +0700

    feat: add official master API route

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AccountingYearController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AuthorizationSourceController;
 use App\Http\Controllers\HealthController;
+use App\Http\Controllers\OfficialAdminController;
 use App\Http\Controllers\ReconciliationController;
 use App\Http\Controllers\ReconciliationCrudController;
 use App\Http\Controllers\SkpdAdminController;
@@ -25,6 +26,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::apiResource('/skpds', SkpdAdminController::class)->only(['index', 'store', 'update']);
     Route::apiResource('/users', UserAdminController::class)->only(['index', 'store', 'update']);
+    Route::apiResource('/officials', OfficialAdminController::class)->only(['index', 'store', 'update']);
 
     Route::get('/authorizations', [AuthorizationSourceController::class, 'index']);
     Route::post('/authorizations', [AuthorizationSourceController::class, 'store']);
