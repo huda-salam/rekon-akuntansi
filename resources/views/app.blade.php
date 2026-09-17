d17567f74d575fca11a0e8ecb95e5cc9727dfd61
huda-salam <38382236+huda-salam@users.noreply.github.com>
2026-09-17 14:46:23 +0700

    feat: add application shell view

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="id">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Rekon Akuntansi</title>
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
