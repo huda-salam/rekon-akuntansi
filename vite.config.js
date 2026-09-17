00cf7caf0da845c4fef907dbce3d1ace00fb6328
huda-salam <38382236+huda-salam@users.noreply.github.com>
2026-09-17 21:25:48 +0700

    fix: use Laravel-compatible Vite manifest output

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'public/build',
-    manifest: 'manifest.json',
+    manifest: true,
     emptyOutDir: true,
   },
   server: {
