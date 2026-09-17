89ba2f52f39162b041775a4bf00f31c0e205d835
huda-salam <38382236+huda-salam@users.noreply.github.com>
2026-09-17 22:31:47 +0700

    Expose master reference lookup API

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AuthorizationSourceController;
 use App\Http\Controllers\HealthController;
 use App\Http\Controllers\MasterDataImportController;
+use App\Http\Controllers\MasterReferenceController;
 use App\Http\Controllers\OfficialAdminController;
 use App\Http\Controllers\ReconciliationController;
 use App\Http\Controllers\ReconciliationCrudController;
@@ -20,7 +21,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/auth/me', [AuthController::class, 'me']);
     Route::post('/auth/logout', [AuthController::class, 'logout']);
 
-    // Active year is readable by all authenticated users; mutations are admin-only.
     Route::get('/years', [AccountingYearController::class, 'index']);
     Route::post('/years', [AccountingYearAdminController::class, 'store']);
     Route::post('/years/{accountingYear}/activate', [AccountingYearAdminController::class, 'activate']);
@@ -29,6 +29,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('/users', UserAdminController::class)->only(['index', 'store', 'update']);
     Route::apiResource('/officials', OfficialAdminController::class)->only(['index', 'store', 'update']);
     Route::post('/master-data/import', [MasterDataImportController::class, 'store']);
+    Route::get('/master-references', [MasterReferenceController::class, 'index']);
 
     Route::get('/authorizations', [AuthorizationSourceController::class, 'index']);
     Route::post('/authorizations', [AuthorizationSourceController::class, 'store']);
