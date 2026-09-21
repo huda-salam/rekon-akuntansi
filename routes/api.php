14cf6672e6d51aae40333af5ff2494bd077345b7
huda-salam <38382236+huda-salam@users.noreply.github.com>
2026-09-21 09:01:06 +0700

    Add BA Excel download endpoint

--- routes/api.php
+++ routes/api.php
@@ -51,5 +51,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/reconciliations/{reconciliation}', [ReconciliationCrudController::class, 'update']);
     Route::get('/reconciliations/{reconciliation}/ba', [ReconciliationController::class, 'ba']);
     Route::get('/reconciliations/{reconciliation}/ba/document', [ReconciliationController::class, 'baDocument']);
+    Route::get('/reconciliations/{reconciliation}/ba/excel', [ReconciliationController::class, 'baExcel']);
     Route::post('/reconciliations/{reconciliation}/finalize', [ReconciliationController::class, 'finalize']);
 });
