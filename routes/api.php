9da5a5943a9fc724e5a89daed279995f9990913e
huda-salam <38382236+huda-salam@users.noreply.github.com>
2026-09-21 08:59:27 +0700

    Add structured BA document endpoint

--- routes/api.php
+++ routes/api.php
@@ -50,5 +50,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/reconciliations/{reconciliation}', [ReconciliationController::class, 'show']);
     Route::put('/reconciliations/{reconciliation}', [ReconciliationCrudController::class, 'update']);
     Route::get('/reconciliations/{reconciliation}/ba', [ReconciliationController::class, 'ba']);
+    Route::get('/reconciliations/{reconciliation}/ba/document', [ReconciliationController::class, 'baDocument']);
     Route::post('/reconciliations/{reconciliation}/finalize', [ReconciliationController::class, 'finalize']);
 });
